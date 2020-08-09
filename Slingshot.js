405864c4e7591b83712d58419b3d4450e60698fa
Srijan-sarkar <68334108+Srijan-sarkar@users.noreply.github.com>
2020-08-09 18:41:27 +0530

    Add files via upload

--- Slingshot.js
+++ Slingshot.js
@@ -3,7 +3,7 @@ class SlingShot{
         var options = {
             bodyA: bodyA,
             pointB: pointB,
-            stiffness: 0.01,
+            stiffness: 0.04,
             length: 10
         }
 
